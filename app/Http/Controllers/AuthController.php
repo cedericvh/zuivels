3afd9016afdc36fa48ebc22074648833475aafe7
daviddev <david1994martirosyan@gmail.com>
2018-06-14 16:04:28 +0400

    fix register

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Role;
 use App\Models\User;
 use App\Http\Requests\RegisterRequest as Request;
 use Laravel\Passport\Http\Controllers\AccessTokenController;
